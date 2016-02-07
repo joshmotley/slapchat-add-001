05b4ff326a7912c5c87e8fe438264cfff4ee2acf
joshmotley <joshkmotley@gmail.com>
2016-02-06 18:51:42 -0600

    set up core data,data model,test message

---
+++ FISMessage.m
@@ -0,0 +1,15 @@
+//
+//  FISMessage.m
+//  slapChat
+//
+//  Created by Joshua Motley on 2/6/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import "FISMessage.h"
+
+@implementation FISMessage
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
